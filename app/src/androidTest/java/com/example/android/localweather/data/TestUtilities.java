fab865c95bc0c85aa302199556db618438c01cb0
Sinisa Vukovic <sinisa503@gmail.com>
2016-03-16 15:38:16 +0100

    Refactoring-2

--- app/src/androidTest/java/com/example/android/localweather/data/TestUtilities.java
+++ app/src/androidTest/java/com/example/android/localweather/data/TestUtilities.java
@@ -15,11 +15,6 @@ import com.example.android.localweather.utils.PollingCheck;
 import java.util.Map;
 import java.util.Set;
 
-/*
-    Students: These are functions and some test com.example.android.sunshine.data to make it easier to test your database and
-    Content Provider.  Note that you'll want your WeatherContract class to exactly match the one
-    in our solution to use these as-given.
- */
 public class TestUtilities extends AndroidTestCase {
     static final String TEST_LOCATION = "99705";
     static final long TEST_DATE = 1419033600L;  // December 20th, 2014
@@ -43,9 +38,7 @@ public class TestUtilities extends AndroidTestCase {
         }
     }
 
-    /*
-        Students: Use this to create some default weather values for your database tests.
-     */
+
     static ContentValues createWeatherValues(long locationRowId) {
         ContentValues weatherValues = new ContentValues();
         weatherValues.put(WeatherContract.WeatherEntry.COLUMN_LOC_KEY, locationRowId);
@@ -62,12 +55,8 @@ public class TestUtilities extends AndroidTestCase {
         return weatherValues;
     }
 
-    /*
-        Students: You can uncomment this helper function once you have finished creating the
-        LocationEntry part of the WeatherContract.
-     */
+
     static ContentValues createNorthPoleLocationValues() {
-        // Create a new map of values, where column names are the keys
         ContentValues testValues = new ContentValues();
         testValues.put(WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING, TEST_LOCATION);
         testValues.put(WeatherContract.LocationEntry.COLUMN_CITY_NAME, "North Pole");
@@ -77,12 +66,7 @@ public class TestUtilities extends AndroidTestCase {
         return testValues;
     }
 
-    /*
-        Students: You can uncomment this function once you have finished creating the
-        LocationEntry part of the WeatherContract as well as the WeatherDbHelper.
-     */
     static long insertNorthPoleLocationValues(Context context) {
-        // insert our test records into the database
         WeatherDbHelper dbHelper = new WeatherDbHelper(context);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues testValues = TestUtilities.createNorthPoleLocationValues();
@@ -90,19 +74,11 @@ public class TestUtilities extends AndroidTestCase {
         long locationRowId;
         locationRowId = db.insert(WeatherContract.LocationEntry.TABLE_NAME, null, testValues);
 
-        // Verify we got a row back.
         assertTrue("Error: Failure to insert North Pole Location Values", locationRowId != -1);
 
         return locationRowId;
     }
 
-    /*
-        Students: The functions we provide inside of TestProvider use this utility class to test
-        the ContentObserver callbacks using the PollingCheck class that we grabbed from the Android
-        CTS tests.
-        Note that this only tests that the onChange function is called; it does not test that the
-        correct Uri is returned.
-     */
     static class TestContentObserver extends ContentObserver {
         final HandlerThread mHT;
         boolean mContentChanged;
@@ -118,7 +94,6 @@ public class TestUtilities extends AndroidTestCase {
             mHT = ht;
         }
 
-        // On earlier versions of Android, this onChange method is called
         @Override
         public void onChange(boolean selfChange) {
             onChange(selfChange, null);
@@ -130,10 +105,6 @@ public class TestUtilities extends AndroidTestCase {
         }
 
         public void waitForNotificationOrFail() {
-            // Note: The PollingCheck class is taken from the Android CTS (Compatibility Test Suite).
-            // It's useful to look at the Android CTS source for ideas on how to test your Android
-            // applications.  The reason that PollingCheck works is that, by default, the JUnit
-            // testing framework is not running on the main Android application thread.
             new PollingCheck(5000) {
                 @Override
                 protected boolean check() {
